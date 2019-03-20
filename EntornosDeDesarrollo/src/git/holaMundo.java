355a99041680e6bbf84ab9ecf9cbc5f3b04fdc70
Vespertino <Vespertino@C8PC-5>
2019-03-20 21:15:38 +0100

    Nueva version

--- EntornosDeDesarrollo/src/git/holaMundo.java
+++ EntornosDeDesarrollo/src/git/holaMundo.java
@@ -3,6 +3,6 @@ package git;
 public class holaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("V2.0");
+		System.out.println("Modificado");
 	}
 }
